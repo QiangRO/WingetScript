1b85e11e9b530ed2a7d1828d158a22a0f02d4880
QiangRO <aroch22222@hotmail.com>
2025-03-27 11:41:11 -0400

    Formato de archivo

--- setup.ps1
+++ setup.ps1
@@ -5,17 +5,6 @@ param (
 $scriptPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $First = Join-Path -Path $scriptPath -ChildPath "1.Inicializer-File.ps1"
 
-
-function Write-Message {
-    Write-Host "Hola $scriptPath" -ForegroundColor Cyan
-    Write-Host "Hola $scriptPath2" -ForegroundColor Cyan
-    Write-Host "Hola $scriptIdProgramPath" -ForegroundColor Cyan
-}
-
-function Write-Message2 {
-    Write-Host "Mundo" -ForegroundColor Cyan
-}
-
 function Test-Functions {
     foreach ($FunctionName in $FunctionNames) {
         if (Get-Command -Name $FunctionName -ErrorAction SilentlyContinue) {
